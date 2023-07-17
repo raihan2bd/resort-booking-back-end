1db25b534fbeceb084d28a306713b9d82421fe66
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-07-17 11:26:27 +0200

    Add user_id to resort migration

--- db/migrate/20230717090553_create_resorts.rb
+++ db/migrate/20230717090553_create_resorts.rb
@@ -1,6 +1,7 @@
 class CreateResorts < ActiveRecord::Migration[7.0]
   def change
     create_table :resorts do |t|
+      t.references :user_id, foreign_key: true
       t.string :name
       t.string :location
       t.float :price
