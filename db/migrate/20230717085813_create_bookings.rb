29ae6a4414743dcfaf4657ae5788e732060c9bc8
MICHAELKITH <michaelkithinji27@gmail.com>
2023-07-17 18:30:35 +0300

    Update bookings table

--- db/migrate/20230717085813_create_bookings.rb
+++ db/migrate/20230717085813_create_bookings.rb
@@ -1,9 +1,10 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.datetime :start_time
-      t.datetime :end_time
+      t.datetime :start_date
+      t.datetime :end_date
       t.references :user, null: false, foreign_key: true, on_delete: :cascade
+      
 
       t.timestamps
     end
