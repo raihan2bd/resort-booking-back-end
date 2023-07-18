c6254f61e2163bd5c3f2ef8da4c0bc48f03cf052
MICHAELKITH <michaelkithinji27@gmail.com>
2023-07-18 12:03:38 +0300

    Update booking model

--- db/migrate/20230717085813_create_bookings.rb
+++ db/migrate/20230717085813_create_bookings.rb
@@ -3,8 +3,9 @@ class CreateBookings < ActiveRecord::Migration[7.0]
     create_table :bookings do |t|
       t.datetime :start_date
       t.datetime :end_date
+      t.string :address
       t.references :user, null: false, foreign_key: true, on_delete: :cascade
-      
+      t.references :resort, null: false, foreign_key: true, on_delete: :cascade
 
       t.timestamps
     end
