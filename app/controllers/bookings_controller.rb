183ed23ab9e502ee880f7ccdaade096efb82ccb1
edubew <edubew98@gmail.com>
2023-07-19 16:05:39 +0300

    Update the destroy method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,8 +13,12 @@ class BookingsController < ApplicationController
   end
 
   def destroy
-    # create delete method
-    @booking.destroy
+    @booking = Booking.find_by_id(params[:id])
+    if @booking.destroy
+      render json: { message: 'Booking removed sucessfully' }, status: :ok
+    else
+      render json: { message: "Sorry, couldn't remove booking" }, status: :unprocessable_entity
+    end 
   end
 
   private
