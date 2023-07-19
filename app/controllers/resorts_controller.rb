60cf8755cdb0a77e9de9d550f0181964b2d0346b
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-19 09:16:27 +0100

    Add create method

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -11,6 +11,15 @@ class ResortsController < ApplicationController
     render json: package
   end
 
+  def create
+    resort = Resort.new(resort_params)
+    if resort.save
+      render json: { message: 'Resort created' }, status: :created
+    else
+      render json: { error: 'Unable to create resort' }, status: :unprocessable_entity
+    end
+  end
+  
   private
 
   def resort_params
