76072768494a2508cfa504f17a5802e68144ac66
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-19 09:11:10 +0100

    Create index, show, create method for resorts

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,5 +1,13 @@
 class ResortsController < ApplicationController
+
+
   def index
-    @resorts = Resort.all
+    resorts = Resort.include(:user)
+    render json: resorts
+  end
+
+  def show
+    resort = Presort.includes(:user).find(params[:id])
+    render json: packages
   end
 end
