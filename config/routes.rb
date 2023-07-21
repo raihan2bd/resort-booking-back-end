8827d9e8ceaf63bd11e93f673308c4f82c054f38
raihan2bd <raihan2bd.official@gmail.com>
2023-07-21 15:13:52 +0600

    Configure the project devise for jwt authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get '/current_user', to: 'current_user#index'
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   resources :resorts, only: [:index, :show, :create, :destroy]
   resources :bookings, only: [:index, :create, :destroy]
 end
