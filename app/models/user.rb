ee2511be334e6a6b4a9393735fba87ad30657c5d
edubew <edubew98@gmail.com>
2023-07-24 07:27:36 +0000

    Add validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  # Validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :role, inclusion: { in: %w[member admin] }
+
+  # Associations
   has_many :resorts, dependent: :destroy
   has_many :bookings, dependent: :destroy
-
-  validates :name, presence: true
 end
