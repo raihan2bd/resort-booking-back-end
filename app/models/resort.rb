0e6a63f3b05b87bd88f0db49c71557a390dbdcf9
edubew <edubew98@gmail.com>
2023-07-24 07:28:52 +0000

    Add validations to the resort model

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,4 +1,14 @@
 class Resort < ApplicationRecord
+
+  # Validations
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :guests_amount, presence: true, numericality: { greater_than: 0 }
+  validates :image_url, presence: true
+
+  # Associations
   belongs_to :user
   has_many :bookings, dependent: :destroy
 end
