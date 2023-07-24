4ad6a593b78ce848c37488c437b3776fb8dce12e
edubew <edubew98@gmail.com>
2023-07-24 07:29:50 +0000

    Add validations to the booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,22 @@
 class Booking < ApplicationRecord
+
+  # Validations
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :address, presence: true
+
+  # Custom validation to check if the start date is before the end date
+  validate :start_date_before_end_date
+
+  # Associations
   belongs_to :user
   belongs_to :resort
+
+  private
+
+  def start_date_before_end_date
+    return unless start_date && end_date
+
+    errors.add(:start_date, "must be before the end date") if start_date >= end_date
+  end
 end
